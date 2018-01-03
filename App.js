7056044ffa211c34516e0f88ad5f73aeeefd930f
Erik <eguzmail@gmail.com>
2018-01-02 19:55:39 -0800

    Include in roboto font

--- App.js
+++ App.js
@@ -9,20 +9,46 @@ import { Provider } from 'react-redux'
 import store from './app/redux/configureStore'
 
 export default class TwitchDashboardApp extends Component {
+
   constructor() {
     super();
+    this.state = {
+      ready: false,
+    }
 
     if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) { 
       UIManager.setLayoutAnimationEnabledExperimental(true); 
     }
   }
 
+  async componentWillMount() {
+
+    if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) { 
+      await Expo.Font.loadAsync({
+        'Roboto': require('native-base/Fonts/Roboto.ttf'),
+        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      });
+    }
+
+    this.setState({
+      ready: true
+    });
+  }
+
+  Setup() {
+    if (this.state.ready) {
+      return (
+        <Provider store={store}>
+          <ReduxNavigation />
+        </Provider>
+      );
+    } else {
+      return '';      
+    }
+  }
+
   render() {
-    return (
-      <Provider store={store}>
-        <ReduxNavigation />
-      </Provider>
-    );
+    return this.Setup();
   }
 };
 
