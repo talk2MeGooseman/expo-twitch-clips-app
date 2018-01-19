5d53899179956b7e3da9527972b214d36a738e7d
Erik Guzman <me.erik@me.com>
2018-01-19 15:45:00 -0800

    Work to trim down reducers

--- app/redux/CombineReducers.js
+++ app/redux/CombineReducers.js
@@ -1,4 +1,6 @@
-import { AsyncStorage } from 'react-native';
+import {
+  AsyncStorage
+} from 'react-native';
 import AppNavigation from '../navigation/AppNavigation';
 import {
   combineReducers
@@ -38,15 +40,21 @@ import {
   CHANNEL_FOLLOWERS_RESPONSE,
   REQUESTING_CHANNELS_FOLLOWERS,
 } from "./actions/userStuffAction";
+import {
+  ADD_BOOKMARK,
+  REMOVE_BOOKMARK,
+  INIT_BOOKMARKS
+} from './actions/bookmarkActions';
 import CONSTANTS from '../lib/Constants';
-import { ADD_BOOKMARK, REMOVE_BOOKMARK, INIT_BOOKMARKS } from './actions/bookmarkActions';
 
 const navReducer = (state, action) => {
   const newState = AppNavigation.router.getStateForAction(action, state)
   return newState || state
 }
 
-function authTwitchApp(state = { loggedIn: false }, action) {
+function authTwitchApp(state = {
+  loggedIn: false
+}, action) {
   switch (action.type) {
     case AUTH_USER:
     case USER_AUTHED:
@@ -56,98 +64,194 @@ function authTwitchApp(state = { loggedIn: false }, action) {
   }
 }
 
-function userFollowing(state = { following: [], total: 0, loading: false, refreshing: false }, action) {
+const userFollowingDefault = {
+  following: [],
+  total: 0,
+  loading: false,
+  refreshing: false
+};
+function userFollowing(state = userFollowingDefault, action) {
   switch (action.type) {
     case FILTER_FOLLOWING:
-      return Object.assign({}, state, { filter: action.filter });
+      return Object.assign({}, state, {
+        filter: action.filter
+      });
     case REFRESH_FOLLOWING:
-      return Object.assign({}, state, { refreshing: true, following: [] });    
+      return Object.assign({}, state, {
+        refreshing: true,
+        following: []
+      });
     case FETCHING_FOLLOWING:
-      return Object.assign({}, state, { loading: true, following: [] });
+      return Object.assign({}, state, {
+        loading: true,
+        following: []
+      });
     case FOLLOWING_RESPONSE:
-      return Object.assign({}, state, { following: action.following, total: action.total, loading: false, refreshing: false });
+      return Object.assign({}, state, {
+        following: action.following,
+        total: action.total,
+        loading: false,
+        refreshing: false
+      });
     default:
       return state;
   }
 }
 
-function userClips(state = { clips: [], cursor: '', loading: false, refreshing: false }, action) {
+const userClipsDefault = {
+  clips: [],
+  cursor: '',
+  loading: false,
+  refreshing: false
+};
+function userClips(state = userClipsDefault, action) {
   switch (action.type) {
     case REFRESH_USER_CLIPS:
-      return Object.assign({}, state, { clips: [], refreshing: true});
+      return Object.assign({}, state, {
+        clips: [],
+        refreshing: true
+      });
     case FETCHING_USER_CLIPS:
-      return Object.assign({}, state, { loading: true });
+      return Object.assign({}, state, {
+        loading: true
+      });
     case USER_CLIPS_RESPONSE:
-      return Object.assign({}, state, { clips: state.clips.concat(action.clips), cursor: action.cursor, loading: false, refreshing: false });
+      return Object.assign({}, state, {
+        clips: state.clips.concat(action.clips),
+        cursor: action.cursor,
+        loading: false,
+        refreshing: false
+      });
     default:
       return state;
   }
 }
 
-function userVideoActions(scope='generic') {
-  return function userVideos(state = { videos: [], total: 0, loading: false, refreshing: false }, action) {
-    if(scope != action.scope) {
+const userVideoActionsDefault = {
+  videos: [],
+  total: 0,
+  loading: false,
+  refreshing: false
+};
+// Wrap Reducer in a function so it can be scoped and reused across views
+function userVideoActions(scope = 'generic') {
+  return function userVideos(state = userVideoActionsDefault, action) {
+    if (scope != action.scope) {
       return state;
     }
 
     switch (action.type) {
       case REFRESH_USER_VIDEOS:
-        return Object.assign({}, state, { videos: [], refreshing: true });
+        return Object.assign({}, state, {
+          videos: [],
+          refreshing: true
+        });
       case FETCHING_USER_VIDEOS:
-        return Object.assign({}, state, { loading: true });
+        return Object.assign({}, state, {
+          loading: true
+        });
       case USER_VIDEOS_RESPONSE:
-        return Object.assign({}, state, { videos: state.videos.concat(action.videos), total: action.total, loading: false, refreshing: false });
+        return Object.assign({}, state, {
+          videos: state.videos.concat(action.videos),
+          total: action.total,
+          loading: false,
+          refreshing: false
+        });
       default:
         return state;
     }
   }
 }
 
-function topClips(state = { top_clips: [], trending_clips: [], suggested_count: 25, trending_count: 25, loading: true, refreshing: false }, action) {
+const topClipsDefault = {
+  top_clips: [],
+  trending_clips: [],
+  suggested_count: 25,
+  trending_count: 25,
+  loading: true,
+  refreshing: false
+};
+
+function topClips(state = topClipsDefault, action) {
   switch (action.type) {
     case FETCHING_SUGGESTED_TOP_CLIPS:
-      return Object.assign({}, state, { loading: true, top_clips: []});
+      return Object.assign({}, state, {
+        loading: true,
+        top_clips: []
+      });
     case FETCHING_TRENDING_CLIPS:
-      return Object.assign({}, state, { loading: true, trending_clips: []});
+      return Object.assign({}, state, {
+        loading: true,
+        trending_clips: []
+      });
     case TRENDING_CLIPS_COUNT:
       return Object.assign({}, state, action);
     case SUGGESTED_TOP_CLIPS_COUNT:
       return Object.assign({}, state, action);
     case SUGGESTED_TOP_CLIPS_RESPONSE:
-      return Object.assign({}, state, { loading: false, refreshing: false }, action);
+      return Object.assign({}, state, {
+        loading: false,
+        refreshing: false
+      }, action);
     case TRENDING_CLIPS_RESPONSE:
-      return Object.assign({}, state, { loading: false, refreshing: false }, action);
+      return Object.assign({}, state, {
+        loading: false,
+        refreshing: false
+      }, action);
     default:
       return state;
   }
 }
 
-function userStuff(state = { userInfo: {}, loadingUserInfo: false, totalFollowers: null, follows: [], cursor: '', loadingFollowers: false }, action) {
+const userStuffDefault = {
+  userInfo: {},
+  loadingUserInfo: false,
+  totalFollowers: null,
+  follows: [],
+  cursor: '',
+  loadingFollowers: false
+};
+
+function userStuff(state = userStuffDefault, action) {
   switch (action.type) {
     case SET_USERS_INFO:
-      return Object.assign({}, state, action, { loadingUserInfo: false });
+      return Object.assign({}, state, action, {
+        loadingUserInfo: false
+      });
     case REQUESTING_USER_INFO:
-      return Object.assign({}, state, { loadingUserInfo: true });
+      return Object.assign({}, state, {
+        loadingUserInfo: true
+      });
     case REQUESTING_CHANNELS_FOLLOWERS:
-      return Object.assign({}, state, { loadingFollowers: true });
+      return Object.assign({}, state, {
+        loadingFollowers: true
+      });
     case CHANNEL_FOLLOWERS_RESPONSE:
-      return Object.assign({}, state, action, { loadingFollowers: false, follows: state.follows.concat(action.follows) });
+      return Object.assign({}, state, action, {
+        loadingFollowers: false,
+        follows: state.follows.concat(action.follows)
+      });
     default:
       return state;
   }
 }
 
-function bookmarks(state = { bookmarks: {}}, action) {
+const bookmarksDefault = {
+  bookmarks: {}
+};
+
+function bookmarks(state = bookmarksDefault, action) {
   let newState = JSON.parse(JSON.stringify(state));
-  let data =  Object.assign({}, action.data);
+  let data = Object.assign({}, action.data);
 
   switch (action.type) {
-    case ADD_BOOKMARK: {
-      newState.bookmarks[data.id] = data;
-      AsyncStorage.setItem('TWITCH:BOOKMARKS:key', JSON.stringify(newState));
-      return newState;
-    }
-    case REMOVE_BOOKMARK: 
+    case ADD_BOOKMARK:
+      {
+        newState.bookmarks[data.id] = data;
+        AsyncStorage.setItem('TWITCH:BOOKMARKS:key', JSON.stringify(newState));
+        return newState;
+      }
+    case REMOVE_BOOKMARK:
       delete newState.bookmarks[data.id];
       AsyncStorage.setItem('TWITCH:BOOKMARKS:key', JSON.stringify(newState));
       return newState;
